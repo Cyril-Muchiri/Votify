63a42173132bad6e60052880a457b55b4c87ea93
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-11-05 12:33:29 +0300

    extended the http generic class

---
+++ src/main/java/com/votifysoft/servlet/AppB.java
@@ -0,0 +1,16 @@
+package com.votifysoft.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AppB extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        PrintWriter writer=res.getWriter();
+        writer.write("<h1>This servlet extends generic http servlet class</h1>");
+    }
+}
