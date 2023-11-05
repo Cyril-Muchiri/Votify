21480e267c953e3cd189cdb45a3e3f2308d4da9b
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-11-06 02:55:33 +0300

    working on updating my code for votesrvlet

---
+++ src/main/java/com/votifysoft/appservlet/VoteServlet.java
@@ -0,0 +1,33 @@
+package com.votifysoft.appservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import com.votifysoft.repository.PersonRepository;
+
+@WebServlet("/vote")
+public class VoteServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        PersonRepository.voteForPerson(id);
+
+        System.out.println("This servlet is used");
+
+        response.sendRedirect("./app/home.html");
+
+    }
+
+}
